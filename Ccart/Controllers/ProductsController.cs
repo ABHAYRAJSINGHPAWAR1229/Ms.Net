bfd647f1eb23e01e4b8f719576d0538e41a68f6f
Thakur Abhay Raj Singh Pawar <abhayrajsinghpawar.cdac@gmail.com>
2024-07-28 18:45:05 +0530

    first Ms.net

---
+++ Ccart/Controllers/ProductsController.cs
@@ -0,0 +1,110 @@
+﻿using Ccart.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ccart.Controllers
+{
+    public class productsController : Controller
+    {
+        Product pr = new Product();
+        // GET: productsController
+        public ActionResult Index()
+        {
+            
+            List<Product> pro=pr.getAllproduct();
+            return View(pro);
+        }
+
+        public ActionResult Welcome()
+        {
+
+            
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+
+           
+            return View();
+        }
+
+        // GET: productsController/Details/5
+        public ActionResult Details(int id)
+        {
+            Product pro = pr.getSingleproduct(id);
+            return View(pro);
+        }
+
+        // GET: productsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: productsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product pro)
+        {
+            try
+            {
+                pr.createproduct(pro);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: productsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Product pro = pr.getSingleproduct(id);
+            return View(pro);
+        }
+
+        // POST: productsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product pro)
+        {
+            try
+            {
+                pr.updateProduct(pro);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: productsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Product pro = pr.getSingleproduct(id);
+            Console.WriteLine("first");
+            return View(pro);
+        }
+
+        // POST: productsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                Console.WriteLine("before delete");
+                pr.deleteProduct(id);
+                Console.WriteLine("deleted After Controller");
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Ccart/Models/Product.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+
+namespace Ccart.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Boolean Availability { get; set; }   
+
+        public static List<Product> list=Populating.populate();
+
+        public static int i=1;
+        
+
+        public  List<Product>  getAllproduct()
+        {
+            return list;
+        }
+        public Product getSingleproduct(int id)
+        {
+            
+            Product product = null;
+            foreach (var p in list)
+            {
+                if (p.Id == id)
+                {
+                    product = p;
+                    break;
+                }
+            }
+
+            return product;
+        }
+
+        public void updateProduct(Product product)
+        {
+            int id = product.Id;
+            list[id-1]= product;
+        }
+
+        public void createproduct(Product product)
+        {
+            
+            list.Add(product);
+
+        }
+
+        public void deleteProduct(int id)
+        {
+        
+            foreach (Product p in list)
+            {
+                if(p.Id == id)
+                {
+                    Console.WriteLine(list.Contains(p));
+                  
+                    list.Remove(p);
+                    Console.WriteLine(list.Contains(p));
+                    break;
+                }
+                
+                
+            }
+          
+        }
+       
+
+    }
+
+    public class Populating
+    {
+        public static List<Product> populate()
+        {
+
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product { Id = 1, Name = "Garnier Cream", Description = "for facial purpose", Availability = true });
+            list.Add(new Product { Id = 2, Name = "Glow & lovely Cream", Description = "for facial purpose", Availability = true });
+            list.Add(new Product { Id = 3, Name = "Nivea Cream", Description = "for facial purpose", Availability = true });
+            list.Add(new Product { Id = 4, Name = "Ponds White beauty Cream", Description = "for facial purpose", Availability = true });
+            list.Add(new Product { Id = 5, Name = "himami Cream", Description = "for facial purpose", Availability = true });
+
+
+
+            return list;
+        }
+    }
+   
+}
